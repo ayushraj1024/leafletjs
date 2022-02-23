72e6ada064da34ec049afabd16bc54d9d2c5a77c
= <ayushraj1024@gmail.com>
2022-02-23 19:07:00 +0530

    Add R support

--- r_function.php
+++ r_function.php
@@ -2,5 +2,5 @@
 echo shell_exec("Rscript my_script.r {$_GET['range']}");
 // return image tag
 $nocache = rand();
-echo("<img src='temp.png?$nocache' />");
+echo("<img src='/opt/bitnami/apache/htdocs/temp.png?$nocache' />");
 ?>
